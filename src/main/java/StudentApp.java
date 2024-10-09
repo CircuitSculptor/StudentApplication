e68d2e3b63cc5a86832b5ee980a88365a823bf8d
BARTLOMIEJDRAPINSKI- <g00428194@atu.ie>
2024-10-09 16:55:25 +0100

    started writing code

--- src/main/java/StudentApp.java
+++ src/main/java/StudentApp.java
@@ -3,20 +3,18 @@ import java.util.Scanner;
 public class StudentApp {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Please enter a name: ");
-        String name = sc.nextLine();
-        System.out.println("Please enter a email: ");
-        String email = sc.nextLine();
-        System.out.println("Please enter a course: ");
-        String course = sc.nextLine();
+        String[] details1 = getDetails();
 
+        /*
         Student s = new Student();
         s.setName(name);
         s.setEmail(email);
         s.setCourse(course);
 
-        /*
+        Student s2 = new Student("Mary", "mary@atu.ie", "biology" );
+        System.out.println(s);
+        System.out.println(s2);
+
         Student firstStudent = new Student();
         firstStudent.setName("Bartek");
         firstStudent.setEmail("bartek@atu.ie");
@@ -31,4 +29,18 @@ public class StudentApp {
         Student thirdStudent = new Student("Mary", "mary@atu.ie", "biology" );
         */
     }
+    public static String[] getDetails() {
+
+        Scanner sc = new Scanner(System.in);
+        //Prompting for details
+        String[] details = new String[2];
+        System.out.println("Please enter a name: ");
+        String name = sc.nextLine();
+        System.out.println("Please enter a email: ");
+        String email = sc.nextLine();
+        System.out.println("Please enter a course: ");
+        String course = sc.nextLine();
+        return details;
+    }
+
 }
