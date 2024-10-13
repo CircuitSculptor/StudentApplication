c609eff9eafda54a97f2adef06363e1918eda014
Bartlomiej Drapinski <g00428194@atu.ie>
2024-10-13 17:37:51 +0100

    writing code

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -3,16 +3,17 @@ public class Student {
     private String email;
     private String course;
 
+    /*
     public Student() {
-        this.name = null;
-        this.email = null;
-        this.course = null;
+        //this.name = null;
+        //this.email = null;
+        //this.course = null;
     }
 
     public Student(String name) {
         this.name = name;
-        this.email = null;
-        this.course = null;
+        //this.email = null;
+        //this.course = null;
     }
 
     public Student(String name, String email, String course) {
@@ -20,31 +21,38 @@ public class Student {
         this.email = email;
         this.course = course;
     }
-
+    */
 
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
 
-
     public String getEmail() {
         return email;
     }
+
     public void setEmail(String email) {
         this.email = email;
     }
 
-
     public String getCourse() {
         return course;
     }
+
     public void setCourse(String course) {
         this.course = course;
     }
 
+    @Override
+    public String toString() {
+        return "Name: " + name + ", Email: " + email + ", Course: " + course;
+    }
+
+    /*
     @Override
     public String toString() {
         return "Student Details{" +
@@ -53,4 +61,7 @@ public class Student {
                 ", course='" + course + '\'' +
                 '}';
     }
+    */
 }
+
+
--- src/main/java/StudentApp.java
+++ src/main/java/StudentApp.java
@@ -3,44 +3,38 @@ import java.util.Scanner;
 public class StudentApp {
     public static void main(String[] args) {
 
-        String[] details1 = getDetails();
+    String[] details1 = getDetails();
+    String[] details2 = getDetails();
+    String[] details3 = getDetails();
 
-        /*
-        Student s = new Student();
-        s.setName(name);
-        s.setEmail(email);
-        s.setCourse(course);
+    Student s1 = new Student();
+    s1.setName(details[0]);
+    s1.setEmail(email);
+    s1.setCourse(course);
 
-        Student s2 = new Student("Mary", "mary@atu.ie", "biology" );
-        System.out.println(s);
-        System.out.println(s2);
+    Student s2 = new Student();
+    s2.setName(name);
 
-        Student firstStudent = new Student();
-        firstStudent.setName("Bartek");
-        firstStudent.setEmail("bartek@atu.ie");
-        firstStudent.setCourse("Electroncis");
+    System.out.println(s1);
+    System.out.println(s2);
 
-        System.out.println(firstStudent.toString());
+    //System.out.println("Student 1: Name: " + student1.getName() + ", Email: " + student1.getEmail() + ", Course: " + student1.getCourse());
+    //System.out.println("Student 2: Name: " + student2.getName() + ", Email: " + student2.getEmail() + ", Course: " + student2.getCourse());
+    //System.out.println("Student 3: Name: " + student3.getName() + ", Email: " + student3.getEmail() + ", Course: " + student3.getCourse());
 
-        // second student
-        Student secondStudent = new Student("Sean");
-
-        // third student
-        Student thirdStudent = new Student("Mary", "mary@atu.ie", "biology" );
-        */
     }
     public static String[] getDetails() {
-
         Scanner sc = new Scanner(System.in);
-        //Prompting for details
-        String[] details = new String[2];
+
+        //Prompting the user for details
+        String[] details = new String[3];
         System.out.println("Please enter a name: ");
-        String name = sc.nextLine();
+        details[0] = sc.nextLine();
         System.out.println("Please enter a email: ");
         String email = sc.nextLine();
         System.out.println("Please enter a course: ");
-        String course = sc.nextLine();
+        String course= sc.nextLine();
+        System.out.println();
         return details;
     }
-
 }
